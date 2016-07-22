9d5aaa96514f8d5dd57b015f6f4f7d0eb67f94f1
Konstantin Kushnir <chpock@gmail.com>
2016-07-22 12:36:46 +0300

    fix tabs/spaces

--- map.js
+++ map.js
@@ -1,22 +1,22 @@
 function App(){
 
-	$('#map')[0].removeChild($('.cssload-loader')[0]);
-	$('#map')[0].style['background-image'] = 'url(images/background.png)';
+  $('#map')[0].removeChild($('.cssload-loader')[0]);
+  $('#map')[0].style['background-image'] = 'url(images/background.png)';
 
   this.layers = {
     'Google': new L.Google('ROADMAP'),
     'Яндекс': new L.Yandex(),
     'Visicom': new L.TileLayer('http://tms{s}.visicom.ua/2.0.0/planet3/base_ru/{z}/{x}/{y}.png',{
-    	maxZoom: 19,
-    	tms: true,
-    	subdomains: '123'
-  	})
+      maxZoom: 19,
+      tms: true,
+      subdomains: '123'
+    })
   };
 
   this.map = new L.Map('map', {
     center: new L.LatLng(48.502275,34.62719),
     zoom: 11,
-   	zoomControl: false
+    zoomControl: false
   })
     .on({
       load: this.autocompleteBound,
@@ -40,38 +40,38 @@ function App(){
   var self = this;
   if ($.localStorage.isSet('tbl_address') && !$.localStorage.isEmpty('tbl_address')) {
     try {
-	  	$.localStorage.get('tbl_address').forEach(function(o){
-	    	o['marker'] = self.add_address(o.location, o.address);
-	      self.tbl_address.push(o);
-	    });
-	  } catch (err) {
-	    console.log('error while loading tbl_address:');
-	    console.log(err);
-	    this.tbl_address = [];
-	  };
+      $.localStorage.get('tbl_address').forEach(function(o){
+        o['marker'] = self.add_address(o.location, o.address);
+        self.tbl_address.push(o);
+      });
+    } catch (err) {
+      console.log('error while loading tbl_address:');
+      console.log(err);
+      this.tbl_address = [];
+    };
     this.redraw_address();
   };
   if ($.localStorage.isSet('tbl_bs') && !$.localStorage.isEmpty('tbl_bs')) {
     try {
-	  	$.localStorage.get('tbl_bs').forEach(function(o){
-	    	o['polygon'] = self.add_bs_polygon(o.location, o.azimut, o.size);
-	    	o['marker'] = self.add_bs_marker(o.location, o.title);
-	      self.tbl_bs.push(o);
-	    });
-	  } catch (err) {
-	    console.log('error while loading tbl_bs:');
-	    console.log(err);
-	    this.tbl_bs = [];
-	  };
+      $.localStorage.get('tbl_bs').forEach(function(o){
+        o['polygon'] = self.add_bs_polygon(o.location, o.azimut, o.size);
+        o['marker'] = self.add_bs_marker(o.location, o.title);
+        self.tbl_bs.push(o);
+      });
+    } catch (err) {
+      console.log('error while loading tbl_bs:');
+      console.log(err);
+      this.tbl_bs = [];
+    };
     this.redraw_bs();
   };
   if ($.localStorage.isSet('map') && !$.localStorage.isEmpty('map')) {
     try {
-	    this.map.setView($.localStorage.get('map')['center'], $.localStorage.get('map')['zoom']);
-	  } catch (err) {
-	    console.log('error while setting map view:');
-	    console.log(err);
-	  };
+      this.map.setView($.localStorage.get('map')['center'], $.localStorage.get('map')['zoom']);
+    } catch (err) {
+      console.log('error while setting map view:');
+      console.log(err);
+    };
   };
 };
 
@@ -100,7 +100,7 @@ App.prototype = {
   },
   resizeBS: function(bs, inc) {
     bs.size = bs.size + inc;
-	  this.map.removeLayer(bs.polygon);
+    this.map.removeLayer(bs.polygon);
     bs.polygon = this.add_bs_polygon(bs.location, bs.azimut, bs.size);  
   },
   buildBS: function(ev) {
@@ -141,10 +141,10 @@ App.prototype = {
         console.log('geocode wrong status: \'' + status);
         return;
       };
-			var address_components = [];
+      var address_components = [];
       for (var j = 0; j < results[0].address_components.length; j++) {
         if (results[0].address_components[j].types[0] == 'street_number' || results[0].address_components[j].types[0] == 'route')
-        	address_components.push(self.stripAddress(results[0].address_components[j].short_name));
+          address_components.push(self.stripAddress(results[0].address_components[j].short_name));
       };
       if (address_components.length) {     
         bs.title = address_components.join(", ");
@@ -156,7 +156,7 @@ App.prototype = {
   autocompleteChange: function(){
     var place = this.autocomplete.getPlace();
     if (!place.geometry || !place.geometry.location) {
-			L.DomUtil.addClass(this.panel.input_address, 'address-notfound');
+      L.DomUtil.addClass(this.panel.input_address, 'address-notfound');
       return;
     };
 
@@ -170,7 +170,7 @@ App.prototype = {
         (place.address_components[2] && place.address_components[2].short_name || '')
       ].join(',');
     } else {
-  		var address = '';
+      var address = '';
     };
 
     var location = L.latLng([place.geometry.location.lat(),place.geometry.location.lng()]);
@@ -197,43 +197,43 @@ App.prototype = {
   add_bs_polygon: function(location, azimut, size) {
     var points = [];
     points.push(location);
-	  var lat = (location.lat * Math.PI) / 180;
-  	var lon = (location.lng * Math.PI) / 180;
-  	var d = parseFloat(size) / 6378100;
-  	var azm_start = azimut - 60;
-  	var azm_end = azimut + 60;
-  	if ( azm_start < 0 ) {
-    	azm_start = 360 + azm_start;
-    	azm_end = 360 + azm_end;
-  	};
-  	for (var x = azm_start; x <= azm_end; x++) { 
+    var lat = (location.lat * Math.PI) / 180;
+    var lon = (location.lng * Math.PI) / 180;
+    var d = parseFloat(size) / 6378100;
+    var azm_start = azimut - 60;
+    var azm_end = azimut + 60;
+    if ( azm_start < 0 ) {
+      azm_start = 360 + azm_start;
+      azm_end = 360 + azm_end;
+    };
+    for (var x = azm_start; x <= azm_end; x++) { 
       var brng = (x % 360) * Math.PI / 180;
-    	var destLat = Math.asin(Math.sin(lat)*Math.cos(d) + Math.cos(lat)*Math.sin(d)*Math.cos(brng));
-    	var destLng = ((lon + Math.atan2(Math.sin(brng)*Math.sin(d)*Math.cos(lat), Math.cos(d)-Math.sin(lat)*Math.sin(destLat))) * 180) / Math.PI;
-     	destLat = (destLat * 180) / Math.PI;
-     	points.push(new L.LatLng(destLat, destLng));
-	  };
-	  var polygon = L.polygon(points,{
-	    stroke: true,
-	    weight: 1,
-	    color: '#004de8',
-	    opacity: 0.62,
-	    fillColor: '#004de8',
-	    fillOpacity: 0.27,
-	    clickable: true
-	  }).addTo(this.map);
+      var destLat = Math.asin(Math.sin(lat)*Math.cos(d) + Math.cos(lat)*Math.sin(d)*Math.cos(brng));
+      var destLng = ((lon + Math.atan2(Math.sin(brng)*Math.sin(d)*Math.cos(lat), Math.cos(d)-Math.sin(lat)*Math.sin(destLat))) * 180) / Math.PI;
+      destLat = (destLat * 180) / Math.PI;
+      points.push(new L.LatLng(destLat, destLng));
+    };
+    var polygon = L.polygon(points,{
+      stroke: true,
+      weight: 1,
+      color: '#004de8',
+      opacity: 0.62,
+      fillColor: '#004de8',
+      fillOpacity: 0.27,
+      clickable: true
+    }).addTo(this.map);
     polygon.on('mouseover',function(){      
       for (var i = 0; i < this.tbl_bs.length; i++)
         if (this.tbl_bs[i].polygon == polygon)
           break;
-   		L.DomUtil.addClass($('#tbl_bs .panel-item')[i], 'panel-item-active');
+      L.DomUtil.addClass($('#tbl_bs .panel-item')[i], 'panel-item-active');
     },this);
     polygon.on('mouseout',function(){
       $('#tbl_bs .panel-item-active').each(function(){
         L.DomUtil.removeClass($(this)[0], 'panel-item-active');
       });
     },this);
-	  return polygon;
+    return polygon;
   },
   add_bs_marker: function(location, title) {
     var marker = L.marker(location, {
@@ -246,7 +246,7 @@ App.prototype = {
       for (var i = 0; i < this.tbl_bs.length; i++)
         if (this.tbl_bs[i].marker == marker)
           break;
-   		L.DomUtil.addClass($('#tbl_bs .panel-item')[i], 'panel-item-active');
+      L.DomUtil.addClass($('#tbl_bs .panel-item')[i], 'panel-item-active');
     },this);
     marker.on('mouseout',function(){
       $('#tbl_bs .panel-item-active').each(function(){
@@ -258,10 +258,10 @@ App.prototype = {
         if (this.tbl_bs[i].marker == marker)
           break;
       this.tbl_bs[i].location = ev.target.getLatLng();
-		  this.map.removeLayer(app.tbl_bs[i].polygon);
-		  this.tbl_bs[i].polygon = this.add_bs_polygon(this.tbl_bs[i].location, this.tbl_bs[i].azimut, this.tbl_bs[i].size);
+      this.map.removeLayer(app.tbl_bs[i].polygon);
+      this.tbl_bs[i].polygon = this.add_bs_polygon(this.tbl_bs[i].location, this.tbl_bs[i].azimut, this.tbl_bs[i].size);
       this.save_current_state('tbl_bs');
-	    this.updateBSTitle(this.tbl_bs[i]);
+      this.updateBSTitle(this.tbl_bs[i]);
     },this);
     return marker;
   },
@@ -275,7 +275,7 @@ App.prototype = {
       for (var i = 0; i < this.tbl_address.length; i++)
         if (this.tbl_address[i].marker == marker)
           break;
-   		L.DomUtil.addClass($('#tbl_address .panel-item')[i], 'panel-item-active');
+      L.DomUtil.addClass($('#tbl_address .panel-item')[i], 'panel-item-active');
     },this);
     marker.on('mouseout',function(){
       $('#tbl_address .panel-item-active').each(function(){
@@ -303,44 +303,44 @@ App.prototype = {
       c1.innerHTML = i+1 + '.'
       c2.innerHTML = o.title;
       c3.innerHTML = o.azimut;
-			var icon = L.DomUtil.create('img', 'panel-item-close', c4);
-			icon.src = 'images/close.png';
-			var dec = L.DomUtil.create('img', 'panel-item-close', c0);
-			dec.src = 'images/left.png';
-			dec.style.padding = '0px 1px 0px 0px';
-			var inc = L.DomUtil.create('img', 'panel-item-close', c0);
-			inc.src = 'images/right.png';
-			L.DomEvent.addListener(line, 'click', function(ev) {
-			  this.map.panTo(o.location);
-			}, self);
-			L.DomEvent
-				.addListener(dec, 'click', L.DomEvent.stopPropagation)
-				.addListener(dec, 'click', L.DomEvent.preventDefault)
-			  .addListener(dec, 'click', function(ev) {
-				  this.resizeBS(o,-50);
-				}, self);
-			L.DomEvent
-				.addListener(inc, 'click', L.DomEvent.stopPropagation)
-				.addListener(inc, 'click', L.DomEvent.preventDefault)
-				.addListener(inc, 'click', function(ev) {
-				  this.resizeBS(o,50);
-				}, self);
-			L.DomEvent.addListener(icon, 'mouseover', function(ev) {
-			  ev.currentTarget.src = 'images/close-hover.png';
-			}, self);
-			L.DomEvent.addListener(icon, 'mouseout', function(ev) {
-			  ev.currentTarget.src = 'images/close.png';
-			}, self);
-			L.DomEvent
-				.addListener(icon, 'click', L.DomEvent.stopPropagation)
-				.addListener(icon, 'click', L.DomEvent.preventDefault)
-				.addListener(icon, 'click', function(ev) {
-				  this.map.removeLayer(o.marker);
-				  this.map.removeLayer(o.polygon);
-					this.tbl_bs.splice(i,1);
-					this.save_current_state('tbl_bs');
-					this.redraw_bs();
-				}, self);
+      var icon = L.DomUtil.create('img', 'panel-item-close', c4);
+      icon.src = 'images/close.png';
+      var dec = L.DomUtil.create('img', 'panel-item-close', c0);
+      dec.src = 'images/left.png';
+      dec.style.padding = '0px 1px 0px 0px';
+      var inc = L.DomUtil.create('img', 'panel-item-close', c0);
+      inc.src = 'images/right.png';
+      L.DomEvent.addListener(line, 'click', function(ev) {
+        this.map.panTo(o.location);
+      }, self);
+      L.DomEvent
+        .addListener(dec, 'click', L.DomEvent.stopPropagation)
+        .addListener(dec, 'click', L.DomEvent.preventDefault)
+        .addListener(dec, 'click', function(ev) {
+          this.resizeBS(o,-50);
+        }, self);
+      L.DomEvent
+        .addListener(inc, 'click', L.DomEvent.stopPropagation)
+        .addListener(inc, 'click', L.DomEvent.preventDefault)
+        .addListener(inc, 'click', function(ev) {
+          this.resizeBS(o,50);
+        }, self);
+      L.DomEvent.addListener(icon, 'mouseover', function(ev) {
+        ev.currentTarget.src = 'images/close-hover.png';
+      }, self);
+      L.DomEvent.addListener(icon, 'mouseout', function(ev) {
+        ev.currentTarget.src = 'images/close.png';
+      }, self);
+      L.DomEvent
+        .addListener(icon, 'click', L.DomEvent.stopPropagation)
+        .addListener(icon, 'click', L.DomEvent.preventDefault)
+        .addListener(icon, 'click', function(ev) {
+          this.map.removeLayer(o.marker);
+          this.map.removeLayer(o.polygon);
+          this.tbl_bs.splice(i,1);
+          this.save_current_state('tbl_bs');
+          this.redraw_bs();
+        }, self);
     });
   },
   redraw_address: function(){
@@ -356,66 +356,66 @@ App.prototype = {
       c3.style.width = '12px';
       c1.innerHTML = i+1 + '.'
       c2.innerHTML = o.title;
-			var icon = L.DomUtil.create('img', 'panel-item-close', c3);      
-			icon.src = 'images/close.png';
-			L.DomEvent.addListener(line, 'click', function(ev) {
-			  this.map.panTo(o.location);
-			}, self);
-			L.DomEvent.addListener(icon, 'mouseover', function(ev) {
-			  ev.currentTarget.src = 'images/close-hover.png';
-			}, self);
-			L.DomEvent.addListener(icon, 'mouseout', function(ev) {
-			  ev.currentTarget.src = 'images/close.png';
-			}, self);
-			L.DomEvent
-				.addListener(icon, 'click', L.DomEvent.stopPropagation)
-				.addListener(icon, 'click', L.DomEvent.preventDefault)
-				.addListener(icon, 'click', function(ev) {			  
-				  this.map.removeLayer(o.marker);
-					this.tbl_address.splice(i,1);
-					this.save_current_state('tbl_address');
-					this.redraw_address();
-				}, self);
+      var icon = L.DomUtil.create('img', 'panel-item-close', c3);      
+      icon.src = 'images/close.png';
+      L.DomEvent.addListener(line, 'click', function(ev) {
+        this.map.panTo(o.location);
+      }, self);
+      L.DomEvent.addListener(icon, 'mouseover', function(ev) {
+        ev.currentTarget.src = 'images/close-hover.png';
+      }, self);
+      L.DomEvent.addListener(icon, 'mouseout', function(ev) {
+        ev.currentTarget.src = 'images/close.png';
+      }, self);
+      L.DomEvent
+        .addListener(icon, 'click', L.DomEvent.stopPropagation)
+        .addListener(icon, 'click', L.DomEvent.preventDefault)
+        .addListener(icon, 'click', function(ev) {        
+          this.map.removeLayer(o.marker);
+          this.tbl_address.splice(i,1);
+          this.save_current_state('tbl_address');
+          this.redraw_address();
+        }, self);
     });
   },
   save_current_state: function(save_type){
     if (this.tbl_address && (!save_type || save_type === 'tbl_address')) {
-	    var save_tbl_address = [];
-	    this.tbl_address.forEach(function(o){
-	      var tmp = {};
-	      for (var k in o) if (k !== 'marker') tmp[k] = o[k];
-	      save_tbl_address.push(tmp);
-	    });
-	    $.localStorage.set('tbl_address', save_tbl_address);
-	  };
+      var save_tbl_address = [];
+      this.tbl_address.forEach(function(o){
+        var tmp = {};
+        for (var k in o) if (k !== 'marker') tmp[k] = o[k];
+        save_tbl_address.push(tmp);
+      });
+      $.localStorage.set('tbl_address', save_tbl_address);
+    };
     if (this.tbl_bs && (!save_type || save_type === 'tbl_bs')) {
-	    var save_tbl_bs = [];
-	    this.tbl_bs.forEach(function(o){
-	      var tmp = {};
-	      for (var k in o) if (k !== 'marker' && k !== 'polygon') tmp[k] = o[k];
-	      save_tbl_bs.push(tmp);
-	    });
-	    $.localStorage.set('tbl_bs', save_tbl_bs);
-	  };
-	  if (this.map && (!save_type || save_type === 'map')) {
-	    var save_map = {
-	      center: this.map.getCenter(),
-	      zoom: this.map.getZoom()
-	    };
-	    $.localStorage.set('map', save_map);
-	  };
+      var save_tbl_bs = [];
+      this.tbl_bs.forEach(function(o){
+        var tmp = {};
+        for (var k in o) if (k !== 'marker' && k !== 'polygon') tmp[k] = o[k];
+        save_tbl_bs.push(tmp);
+      });
+      $.localStorage.set('tbl_bs', save_tbl_bs);
+    };
+    if (this.map && (!save_type || save_type === 'map')) {
+      var save_map = {
+        center: this.map.getCenter(),
+        zoom: this.map.getZoom()
+      };
+      $.localStorage.set('map', save_map);
+    };
   },
   stripAddress: function(str) {
-	  var tbl = {
-  	  " улица":" ул.",
-	    "улица ":"ул.",
-  	  "проспект":"пр-т",
-    	"бульвар":"б-р",
-	    "переулок ":"пер.",
-  	  " переулок":" пер.",
-	  };
-  	var re = new RegExp(Object.keys(tbl).join("|"),"gi");
-  	return str.replace(re, function(matched){ return tbl[matched]; });
+    var tbl = {
+      " улица":" ул.",
+      "улица ":"ул.",
+      "проспект":"пр-т",
+      "бульвар":"б-р",
+      "переулок ":"пер.",
+      " переулок":" пер.",
+    };
+    var re = new RegExp(Object.keys(tbl).join("|"),"gi");
+    return str.replace(re, function(matched){ return tbl[matched]; });
   }
 };
 
--- tab-panel.js
+++ tab-panel.js
@@ -1,57 +1,57 @@
 L.Control.Panel = L.Control.extend({
-	options: {
-		position: 'topleft',
-		popups: true
-	},
+  options: {
+    position: 'topleft',
+    popups: true
+  },
 
-	initialize: function (options) {
-		L.Util.setOptions(this, options);
-	},
+  initialize: function (options) {
+    L.Util.setOptions(this, options);
+  },
 
-	onAdd: function () {
-	  this.buttons = [];
-	  this.inputs = [];
-	  this.current_button = 0;
+  onAdd: function () {
+    this.buttons = [];
+    this.inputs = [];
+    this.current_button = 0;
 
-		var container = this._container = L.DomUtil.create('div', 'tab-panel');
+    var container = this._container = L.DomUtil.create('div', 'tab-panel');
 
-		var buttonbar = L.DomUtil.create('div', 'tab-panel-buttonbar', container);
+    var buttonbar = L.DomUtil.create('div', 'tab-panel-buttonbar', container);
 
-		this.buttons[0] = L.DomUtil.create('span', 'tab-panel-button tab-panel-button-active', buttonbar);
-		this.buttons[0].innerHTML = 'Поиск адреса';
-		this.buttons[1] = L.DomUtil.create('span', 'tab-panel-button', buttonbar);
-		this.buttons[1].innerHTML = 'Построить БС';
-		L.DomEvent.addListener(this.buttons[0], 'click', function(ev){ 
-		  L.DomEvent.stopPropagation(ev);
-		  this._selectPanel(0);
-		}, this);
-		L.DomEvent.addListener(this.buttons[1], 'click', function(ev){ 
-		  L.DomEvent.stopPropagation(ev);
-		  this._selectPanel(1);
-		}, this);
+    this.buttons[0] = L.DomUtil.create('span', 'tab-panel-button tab-panel-button-active', buttonbar);
+    this.buttons[0].innerHTML = 'Поиск адреса';
+    this.buttons[1] = L.DomUtil.create('span', 'tab-panel-button', buttonbar);
+    this.buttons[1].innerHTML = 'Построить БС';
+    L.DomEvent.addListener(this.buttons[0], 'click', function(ev){ 
+      L.DomEvent.stopPropagation(ev);
+      this._selectPanel(0);
+    }, this);
+    L.DomEvent.addListener(this.buttons[1], 'click', function(ev){ 
+      L.DomEvent.stopPropagation(ev);
+      this._selectPanel(1);
+    }, this);
 
-		var inputbar = this.inputs[0] = L.DomUtil.create('div', 'tab-panel-inputbar', container);
-		L.DomUtil.create('span', 'tab-panel-label', inputbar).innerText = "Адрес:";
-		this.input_address = L.DomUtil.create('input', 'tab-panel-input-address', inputbar);
-		L.DomEvent.addListener(this.input_address, 'keydown', function(ev){
-			L.DomUtil.removeClass(ev.currentTarget, 'address-notfound');
-		}, this);
+    var inputbar = this.inputs[0] = L.DomUtil.create('div', 'tab-panel-inputbar', container);
+    L.DomUtil.create('span', 'tab-panel-label', inputbar).innerText = "Адрес:";
+    this.input_address = L.DomUtil.create('input', 'tab-panel-input-address', inputbar);
+    L.DomEvent.addListener(this.input_address, 'keydown', function(ev){
+      L.DomUtil.removeClass(ev.currentTarget, 'address-notfound');
+    }, this);
 
-		var inputbar = this.inputs[1] = L.DomUtil.create('div', 'tab-panel-inputbar', container);
-		L.DomUtil.create('span', 'tab-panel-label', inputbar).innerText = "Азимут:";
-		L.DomUtil.create('input', 'tab-panel-input-azimut', inputbar).value = '0';
-  	container.removeChild(inputbar);
+    var inputbar = this.inputs[1] = L.DomUtil.create('div', 'tab-panel-inputbar', container);
+    L.DomUtil.create('span', 'tab-panel-label', inputbar).innerText = "Азимут:";
+    L.DomUtil.create('input', 'tab-panel-input-azimut', inputbar).value = '0';
+    container.removeChild(inputbar);
 
-  	L.DomEvent.disableClickPropagation(container);
+    L.DomEvent.disableClickPropagation(container);
 
-		return container;
-	},
+    return container;
+  },
 
-	_selectPanel: function(bid) {
-		L.DomUtil.removeClass(this.buttons[this.current_button], 'tab-panel-button-active');
-  	this._container.removeChild(this.inputs[this.current_button]);
-		L.DomUtil.addClass(this.buttons[bid], 'tab-panel-button-active');
-  	this._container.appendChild(this.inputs[bid]);
-  	this.current_button = bid;
-	}
+  _selectPanel: function(bid) {
+    L.DomUtil.removeClass(this.buttons[this.current_button], 'tab-panel-button-active');
+    this._container.removeChild(this.inputs[this.current_button]);
+    L.DomUtil.addClass(this.buttons[bid], 'tab-panel-button-active');
+    this._container.appendChild(this.inputs[bid]);
+    this.current_button = bid;
+  }
 });
